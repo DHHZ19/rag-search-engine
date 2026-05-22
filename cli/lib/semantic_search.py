9a16bfa5a1fe5941e2c6125954ba7f499ac2b894
Diego Hernandez <diegofootball19@gmail.com>
2026-05-21 20:07:45 -0500

    feat: add verify_embeddings command and search functionality to CLI

--- cli/lib/semantic_search.py
+++ cli/lib/semantic_search.py
@@ -1,4 +1,6 @@
 from sentence_transformers import SentenceTransformer
+import json
+import numpy as np
 
 
 def verify_model():
@@ -19,17 +21,121 @@ def embed_text(text):
     print(f"Dimensions: {embedding.shape[0]}")
 
 
+def verify_embeddings():
+    ss = SemanticSearch()
+
+    with open("data/movies.json", "rb") as file:
+        data = json.load(file)
+
+        documents = list(data["movies"])
+
+        embeddings = ss.load_or_create_embeddings(documents)
+
+        print(f"Number of docs: {len(documents)}")
+        print(
+            f"Embeddings shape: {embeddings.shape[0]} vectors in {embeddings.shape[1]} dimensions"
+        )
+
+
+def buld_query_text(query):
+    ss = SemanticSearch()
+
+    embedding = ss.generate_embedding(query)
+
+    print(f"Query: {query}")
+    print(f"First 3 dimensions: {embedding[:3]}")
+    print(f"Shape: {embedding.shape}")
+
+
+def cosine_similarity(vec1, vec2):
+    dot_product = np.dot(vec1, vec2)
+    norm1 = np.linalg.norm(vec1)
+    norm2 = np.linalg.norm(vec2)
+
+    if norm1 == 0 or norm2 == 0:
+        return 0.0
+
+    return dot_product / (norm1 * norm2)
+
+
 class SemanticSearch:
+    def __init__(self):
+        self.model = SentenceTransformer("all-MiniLM-L6-v2")
+        self.embeddings = None
+        self.documents = None
+        self.document_map = {}
+
     def generate_embedding(self, text):
-        if len(text) == 0:
+        t = text.strip()
+        if len(t) == 0:
             raise ValueError("text can't only be whitespace")
 
         embed_text_list = []
 
-        embed_text_list.append(text)
-        embedding = self.model.encode(embed_text_list)
+        embed_text_list.append(t)
+        embedding = self.model.encode(embed_text_list, show_progress_bar=True)
 
         return embedding[0]
 
-    def __init__(self):
-        self.model = SentenceTransformer("all-MiniLM-L6-v2")
+    def build_embeddings(self, documents):
+        self.documents = documents
+        doc_strings = []
+        for i, doc in enumerate(self.documents, start=1):
+            self.document_map[i] = doc
+            doc_strings.append(f"{doc['title']}: {doc['description']}")
+
+            self.embeddings = self.generate_embedding(doc_strings)
+
+        np.save("cache/movie_embeddings.npy", self.embeddings)
+
+        return self.embeddings
+
+    def load_or_create_embeddings(self, documents):
+        # set docuents, and docmap
+        self.documents = documents
+        doc_strings = []
+        for i, doc in enumerate(self.documents, start=1):
+            self.document_map[i] = doc
+            doc_strings.append(f"{doc['title']}: {doc['description']}")
+
+        # load embeddings if present
+        with open("cache/movie_embeddings.npy", "rb") as f:
+            if f is not None:
+                embeddings = np.load("cache/movie_embeddings.npy")
+                if len(embeddings) == len(documents):
+                    return embeddings
+            return self.build_embeddings(documents=documents)
+
+    def search(self, query, limit=5):
+        if self.embeddings is None:
+            raise ValueError(
+                "No embeddings loaded. Call `load_or_create_embeddings` first."
+            )
+
+        embedding = self.generate_embedding(query)
+
+        similarity_scores = []
+
+        for i, e in enumerate(self.embeddings, start=1):
+            similarity_score = cosine_similarity(embedding, e)
+            touple = (self.document_map[i], similarity_score)
+            similarity_scores.append(touple)
+
+        sorted_similarity_scores = sorted(
+            similarity_scores, key=lambda item: item[1], reverse=True
+        )
+
+        # sorted = np.sort(similarity_scores, axis=1)
+
+        # sorted_similarity_scores = sorted[::-1]
+
+        dic_similarities = []
+        for touples_similarites in sorted_similarity_scores[:limit]:
+            dic_similarities.append(
+                {
+                    "score": touples_similarites[1],
+                    "info": self.document_map[int(touples_similarites[0]["id"])],
+                }
+            )
+
+        return dic_similarities
--- cli/semantic_search_cli.py
+++ cli/semantic_search_cli.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
 import argparse
+import json
 
 from lib.semantic_search import verify_model
 from lib.semantic_search import embed_text
+from lib.semantic_search import verify_embeddings
+from lib.semantic_search import SemanticSearch
 
 
 def main():
@@ -15,15 +18,55 @@ def main():
     embed_command = subparsers.add_parser("embed_text", help="embed")
     embed_command.add_argument("query", type=str, help="Search query")
 
+    subparsers.add_parser(
+        "verify_embeddings", help="verify that the semantic-search model loads"
+    )
+
+    embed_command = subparsers.add_parser("embed_query", help="embed")
+    embed_command.add_argument("query", type=str, help="Search query")
+
+    search_command = subparsers.add_parser("search", help="add a search term")
+    search_command.add_argument("search", type=str, help="Search query")
+    search_command.add_argument(
+        "--limit", type=int, required=False, help="Add a limit to your search"
+    )
+
     args = parser.parse_args()
 
     match args.command:
         case "verify":
             verify_model()
 
+        case "verify_embeddings":
+            verify_embeddings()
+
         case "embed_text":
             text = args.query
             embed_text(text)
+
+        case "embed_query":
+            text = args.query
+            embed_text(text)
+
+        case "search":
+            search = args.search
+            limit = args.limit
+
+            ss = SemanticSearch()
+
+            with open("data/movies.json", "rb") as file:
+                data = json.load(file)
+
+                documents = list(data["movies"])
+
+                ss.embeddings = ss.load_or_create_embeddings(documents)
+
+                res = ss.search(search)
+
+                for i, r in enumerate(res, start=1):
+                    print(
+                        f"{i}. {r['info']['title']} (score: {r['score']}) \n {r['info']['description']} \n"
+                    )
 
         case _:
             parser.print_help()
