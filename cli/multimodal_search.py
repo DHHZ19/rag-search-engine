a33c09ee59e8a434cbbac6cf66438efe58c7fa2f
Diego Hernandez <diegofootball19@gmail.com>
2026-06-04 13:11:32 -0500

    add searching with images

--- cli/multimodal_search.py
+++ cli/multimodal_search.py
@@ -1,19 +1,46 @@
 from PIL import Image
+from search_utils import load_movies
 from sentence_transformers import SentenceTransformer
 
 
 def verify_image_embedding(image_path):
-    ms = MultimodalSearch()
+    movies = load_movies()
+    ms = MultimodalSearch(documents=movies)
     embedding = ms.embed_image(image_path)
     print(f"Embedding shape: {embedding.shape[0]} dimensions")
 
 
+def image_search_command(image_path):
+    movies = load_movies()
+    ms = MultimodalSearch(documents=movies)
+    scores = ms.search_with_image(image_path)
+    return scores
+
+
 class MultimodalSearch:
-    def __init__(self, model_name="clip-ViT-B-32"):
+    def __init__(self, documents: list[dict], model_name="clip-ViT-B-32"):
+        self.documents = load_movies()
+        self.texts = [f"{doc['title']}: {doc['description']}" for doc in self.documents]
         self.st = SentenceTransformer(model_name)
+        self.text_embeddings = self.st.encode(self.texts, show_progress_bar=True)
 
     def embed_image(self, image_path: str):
         image_file = Image.open(image_path)
 
         embedding = self.st.encode(image_file)
         return embedding
+
+    def search_with_image(self, image_path: str):
+        embedding = self.embed_image(image_path)
+
+        res = []
+
+        for doc_embedding in self.text_embeddings:
+            res.append(self.st.similarity(embedding, doc_embedding))
+
+        for i, doc in enumerate(self.documents):
+            res[i] = {"score": res[i], **doc}
+
+        res = sorted(res, key=lambda item: item["score"], reverse=True)
+
+        return res[:5]
--- cli/multimodal_search_cli.py
+++ cli/multimodal_search_cli.py
@@ -3,7 +3,7 @@
 import argparse
 from pathlib import Path
 
-from multimodal_search import verify_image_embedding
+from multimodal_search import image_search_command, verify_image_embedding
 
 
 def main() -> None:
@@ -15,12 +15,25 @@ def main() -> None:
     )
     verify_image.add_argument("verify_image_embedding", type=str, help="add image path")
 
+    image_search = subparsers.add_parser(
+        "image_search", help="search for a movie via an image"
+    )
+    image_search.add_argument("image_search", type=str, help="add image path")
+
     args = parser.parse_args()
 
     match args.command:
         case "verify_image_embedding":
             image_path = args.verify_image_embedding
             verify_image_embedding(image_path)
+
+        case "image_search":
+            image_path = args.image_search
+            scores = image_search_command(image_path)
+
+            for i, s in enumerate(scores, start=1):
+                print(f"{i} {s['title']} (similarity: {s['score'][0][0].item():.1f})")
+                print(f"  {s['description']}")
 
         case _:
             parser.print_help()
